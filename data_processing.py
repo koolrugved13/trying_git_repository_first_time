f3e03c898c5d489faaa2833bbfd63c28a5b54711
koolrugved13 <rugvedraorane03@gmail>
2024-01-24 20:35:05 +0530

    added an new function sum

---
+++ data_processing.py
@@ -0,0 +1,9 @@
+print("adding all the data processing to code")
+
+a=5
+b=7
+def sum(a,b): #we are passing a, b and doing sum of both 
+    c = a+b
+    return c
+print("added new function sum")
+# if youn want to revert previous version
