0f6ca3a4cc711007957b435742a765e64f6970e0
koolrugved13 <rugvedraorane03@gmail>
2024-01-24 20:49:26 +0530

    correct code for Sum function and implemted new mutiplicttion

--- data_processing.py
+++
@@ -1,9 +0,0 @@
-print("adding all the data processing to code")
-
-a=5
-b=7
-def sum(a,b): #we are passing a, b and doing sum of both 
-    c = a+b
-    return c
-print("added new function sum")
-# if youn want to revert previous version
